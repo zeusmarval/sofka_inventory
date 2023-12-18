fe146a9132f7e9fb7e47e35a6ddb20132daf2afc
zeusmarval <zeusmarval@gmail.com>
2023-12-18 10:03:54 -0400

    Initial Spring

---
+++ src/main/java/com/zama/inventarioSofka/InventarioSofkaApplication.java
@@ -0,0 +1,13 @@
+package com.zama.inventarioSofka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventarioSofkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventarioSofkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zama/inventarioSofka/InventarioSofkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zama.inventarioSofka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventarioSofkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
